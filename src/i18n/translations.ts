42ffaa58192d5f916020b4787f61652c6bb9c834
Luard <luard.castellanos@adevinta.com>
2025-04-19 23:50:37 +0200

    Initial commit

---
+++ src/i18n/translations.ts
@@ -0,0 +1,43 @@
+export const languages = {
+  es: 'Castellano',
+  ca: 'Català'
+};
+
+export const defaultLang = 'es';
+
+export const translations = {
+  es: {
+    'nav.home': 'Inicio',
+    'nav.info': 'Información',
+    'nav.rsvp': 'Confirmar',
+    'date.month': 'Septiembre',
+    'date.of': 'de',
+    'rsvp.button': 'Confirmar Asistencia',
+    'info.title': 'Información del evento',
+    'info.date.label': 'Fecha',
+    'info.date.value': '11 de Septiembre, 2025',
+    'info.time.label': 'Hora',
+    'info.location.label': 'Lugar',
+    'info.address.label': 'Dirección',
+    'rsvp.title': 'Confirmar Asistencia',
+    'rsvp.name.label': 'Nombre',
+    'rsvp.surname.label': 'Apellidos',
+  },
+  ca: {
+    'nav.home': 'Inici',
+    'nav.info': 'Informació',
+    'nav.rsvp': 'Confirmar',
+    'date.month': 'Setembre',
+    'date.of': 'de',
+    'rsvp.button': 'Confirmar Assistència',
+    'info.title': 'Informació del event',
+    'info.date.label': 'Data',
+    'info.date.value': '11 de Setembre, 2025',
+    'info.time.label': 'Hora',
+    'info.location.label': 'Lloc',
+    'info.address.label': 'Direcció',
+    'rsvp.title': 'Confirmar Assistència',
+    'rsvp.name.label': 'Nom',
+    'rsvp.surname.label': 'Cognoms',
+  }
+};
